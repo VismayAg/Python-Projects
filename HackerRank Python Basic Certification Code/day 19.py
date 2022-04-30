7f77f75719bb279778fed0c6fddb0856ddca7932
VismayAg <livelikevismay@gmail.com>
2022-04-30 21:59:04 +0530

    day 18 & 19

---
+++ HackerRank Python Basic Certification Code/day 19.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        temp = []
+        for i in range(1, n+1):
+            if n % i == 0:
+                temp.append(i)
+        return sum(temp)
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
